c260da8d84cfe29062ab64f6c12e811d459bc8c2
gameonanil <gameonanil@gmail.com>
2023-06-22 20:05:00 +0545

    refactor: cleanup unused files

--- src/main/kotlin/aakash/day6/day-6-duplicate-array.kt
+++
@@ -1,7 +0,0 @@
-package aakash.day6
-
-class `day-6-duplicate-array` {
-    fun containsDuplicate(nums: IntArray): Boolean {
-        return !nums.distinct().toIntArray().contentEquals(nums)
-    }
-}
