30b013ad7322ec83239e513b78333e956c9a6229
Bibek Shah <bibek.shah@eightsquarei.com>
2023-06-28 18:08:58 +0545

    day 17

---
+++ bibek/017.js
@@ -0,0 +1,9 @@
+checkRecord = s => {
+    let i = -1, a = 0;
+    while (i++ < s.length) {
+        a+= s[i] == 'A';
+        if (s.substring(i - 3, i) == 'LLL') return false;
+        if (a == 2) return false;
+    }
+    return true;
+};
