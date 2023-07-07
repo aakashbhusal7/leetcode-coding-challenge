0fb6dafc397be217f33ddb2d4f8dbcbbc26e8192
aakashbhusal <aakash.bhusal@8squarei.com>
2023-07-07 21:56:18 +0545

    feat: add aakash day 23 solution

---
+++ aakash/day23/day-23-dfs-max-depth-binary-tree.kt
@@ -0,0 +1,22 @@
+/**
+ * Example:
+ * var ti = TreeNode(5)
+ * var v = ti.`val`
+ * Definition for a binary tree node.
+ * class TreeNode(var `val`: Int) {
+ *     var left: TreeNode? = null
+ *     var right: TreeNode? = null
+ * }
+ */
+class Solution {
+    fun maxDepth(root: TreeNode?): Int {
+        if (root == null) return 0
+        val leftHeight = maxDepth(root.left)
+        val rightHeight = maxDepth(root.right)
+        if (leftHeight > rightHeight) {
+            return leftHeight + 1
+        } else {
+            return rightHeight + 1
+        }
+    }
+}
