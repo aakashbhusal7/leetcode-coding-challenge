9d87378c46649fa810f6dcd2a49a23f3b9faf122
Aakash Bhusal <abhusal7@gmail.com>
2024-01-31 22:48:19 +0545

    Time: 202 ms (72.38%), Space: 37.8 MB (92.9%) - LeetHub

--- 0001-two-sum/0001-two-sum.kt
+++ 0001-two-sum/0001-two-sum.kt
@@ -2,7 +2,6 @@ class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
          var output: IntArray = intArrayOf()
         for (i in 0 until nums.size) {
-           //if(Math.abs(nums[i])>target) continue
             for(j in 0 until i){
                 if(nums[i]+nums[j]==target){
                     output= intArrayOf(i,j)
